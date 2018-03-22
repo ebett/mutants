d85460aea2ae2a3b964815eff9b2dc26af1721cb
Esteban Bett <ebett@ats-ar.com.ar>
2018-03-22 08:47:37 -0300

    profiles maven

--- src/main/java/sample/data/rest/MainApplication.java
+++ src/main/java/sample/data/rest/MainApplication.java
@@ -3,13 +3,13 @@ package sample.data.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 /**
  * @author Esteban Bett
  */
 @SpringBootApplication
 @EnableCaching
-public class MainApplication {
+public class MainApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
